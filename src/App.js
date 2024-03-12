c61b5028f8bb1a07415703bfcc31ad743dd51abf
Wellington Silva <wsilva9589@gmail.com>
2024-03-11 21:31:59 -0300

    criando a home e concluindo o botao pesquisa

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import Header from "./componentes/Header";
+import Pesquisa from "./componentes/Pesquisa";
 import styled from "styled-components";
 
 const AppContainer = styled.div`
-    width: 100vw;
-    height: 100vh;
-    background-image: linear-gradient(90deg, #002f52 35%, #326589 100%);
-
-  
-`
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(90deg, #002f52 35%, #326589 100%);
+`;
 
 function App() {
   return (
     <AppContainer>
       <Header />
+      <Pesquisa />
     </AppContainer>
   );
 }
