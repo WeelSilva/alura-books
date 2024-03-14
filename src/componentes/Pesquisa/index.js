8baf92d83c4360826b2035993b78f95da16eb9db
Wellington Silva <wsilva9589@gmail.com>
2024-03-13 22:30:50 -0300

    adicionando uma nova secao de livros

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -4,11 +4,11 @@ import { useState } from "react";
 import { livros } from "./dadosPesquisa";
 
 const PesquisaContainer = styled.section`
-  background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
+  background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
   color: #fff;
   text-align: center;
   padding: 85px 0;
-  height: 270px;
+  height: 470px;
   width: 100%;
 `;
 
